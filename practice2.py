08758c92a773ea53647f84064adf840e431c3e25
taliraj <taliraj@outlook.com>
2023-03-24 10:28:00 +0000

    feat: add practice2 file

---
+++ practice2.py
@@ -0,0 +1,45 @@
+def simple_optionals1(arg):  # change only this line
+    arg = 1
+    arg = None
+    print(arg)
+
+
+def simple_optionals2(arg):  # change only this line
+    arg = 1.0
+    arg = "test"
+    print(arg)
+
+
+def simple_optionals3(arg):  # change only this line
+    arg = True
+    arg = "test"
+    arg = None
+    arg = 1
+    print(arg)
+
+
+def collections1(arg):  # change only this line
+    arg = [1]
+    print(arg)
+
+
+def collections2(arg):  # change only this line
+    arg = {1, 2, 3}
+    print(arg)
+
+
+def collections3(arg):  # change only this line
+    arg = (1, "test", 1.0)
+    print(arg)
+
+
+def collections4(arg):  # change only this line
+    arg = 1
+    arg = (1, "test", 1.0)
+    print(arg)
+
+ 
+def collections5(arg):  # change only this line
+    arg = ("name", "test", 1.0)
+    arg = (1, "test", 1.0)
+    print(arg)

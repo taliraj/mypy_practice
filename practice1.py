1c25ae9c4500b9254cac41f83714381c876c1279
taliraj <taliraj@outlook.com>
2023-03-24 10:24:52 +0000

    feat: add first practice file

---
+++ practice1.py
@@ -0,0 +1,26 @@
+def simple_builtins1(arg):  # change only this line
+    arg = 1
+    print(arg)
+
+
+def simple_builtins2(arg):  # change only this line
+    arg = 1.0
+    print(arg)
+
+
+def simple_builtins3(arg):  # change only this line
+    arg = True
+    print(arg)
+
+
+def simple_builtins4(arg):  # change only this line
+    arg = "test"
+    print(arg)
+
+
+def simple_builtins5(arg):  # change only this line
+    arg = b"test"
+    print(arg)
+
+
+

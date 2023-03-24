a01f52c18de9418166e28a59c66ece76d70f0007
taliraj <taliraj@outlook.com>
2023-03-24 10:32:02 +0000

    feat: add third file

---
+++ practice3.py
@@ -0,0 +1,30 @@
+def mappings1(arg):  # change only this line
+    arg = {1: 2, 3: 4}
+    print(arg)
+
+
+def mappings2(arg):  # change only this line
+    arg = {"a": 1, "b": 2}
+    print(arg)
+
+
+def mappings3(arg):  # change only this line
+    arg = {"a": [1], "b": [2]}
+    print(arg)
+
+
+def mappings4(arg):  # change only this line
+    arg = None    
+    arg = {1: 2, 3: 4, 5: "5"}
+    print(arg)
+
+
+def mappings5(arg):  # change only this line
+    arg = {"test": [1], "test2": [2], 5: "5"}
+    print(arg)
+
+
+def mappings6(arg, arg2=None):  # change only this line
+    arg = {"a": [1, 1.0], "b": [2]}
+    arg2 = {"a": [1, None], "b": [2, "2"]}
+    print(arg, arg2)
